38302e11c07b223361ffef723db652a37bf9c705
hamitseyrek <hamitseyrek@gmail.com>
2021-09-12 23:38:06 +0300

    add note page

---
+++ lib/add_note.dart
@@ -0,0 +1,90 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_todo_list_sqflite/models/category.dart';
+import 'package:flutter_todo_list_sqflite/utils/db_helper.dart';
+
+class AddNote extends StatefulWidget {
+  const AddNote({ Key? key }) : super(key: key);
+
+  @override
+  _AddNoteState createState() => _AddNoteState();
+}
+
+class _AddNoteState extends State<AddNote> {
+
+  var _myFormKey = GlobalKey<FormState>();
+  late List<Category>? _allCategories;
+  late DbHelper? dbHelper;
+  int onSelectCatId = 1;
+  Category? selectedCat;
+
+  @override
+  void initState() {
+    super.initState();
+    _allCategories = [];
+    dbHelper = DbHelper();
+    
+      dbHelper!.getCategories().then((categoryMapList){
+for (Map<String,dynamic> map in categoryMapList) {
+  _allCategories!.add(Category.fromMap(map));
+}
+selectedCat = _allCategories![0];
+setState(() {
+  
+});
+    });
+    
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Icon(Icons.add),
+          Text('New Note'),
+        ],
+      ) ,
+      ),
+      body: Form(
+        key: _myFormKey,
+        child: Column(
+crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Center(
+              child: Container(
+                child: DropdownButtonHideUnderline(
+                  child: DropdownButton<Category>(
+                    hint: Text("Kategori Seç"),
+                    value: selectedCat,
+                    items: getCategories(),
+                  onChanged: (value){
+                    setState(() {
+                      selectedCat = value!;
+                    onSelectCatId = value.id!;
+                    });
+                  },),
+                ),
+                decoration: BoxDecoration(
+                              border: Border.all(
+                                  color: Theme.of(context).primaryColor,
+                                  width: 1),
+                              borderRadius:
+                                  BorderRadius.all(Radius.circular(10))),
+                                  margin: EdgeInsets.all(8),
+                                   padding:
+                              EdgeInsets.only(left: 30,right: 15),
+              ),
+              
+            ),
+      ],)),
+    );
+  }
+
+  List<DropdownMenuItem<Category>> getCategories() {
+    return _allCategories!.map((category) => DropdownMenuItem<Category>(
+      value: category,
+      child: Text(category.name.toString()),
+    )).toList();
+  }
+}
--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,10 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_todo_list_sqflite/add_note.dart';
+import 'package:flutter_todo_list_sqflite/models/category.dart';
+import 'package:flutter_todo_list_sqflite/utils/db_helper.dart';
 
 class HomePage extends StatelessWidget {
-  HomePage({ Key? key }) : super(key: key);
 
-  var _myFormKey = GlobalKey();
-  @override
+  final DbHelper dbHelper = DbHelper();
+
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text('Notes List'),),
@@ -12,62 +14,81 @@ class HomePage extends StatelessWidget {
       mainAxisAlignment: MainAxisAlignment.end,
       children: [
       FloatingActionButton(onPressed: (){
-        showDialog(
-          context: context,
-          barrierColor: Colors.black.withOpacity(0.4),
-          barrierDismissible: false,
-          builder: (context){
-          return SimpleDialog(
-            title: Text('Add Category', style: TextStyle(color: Theme.of(context).primaryColor)),
-            children: [
-              Form(child: Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: TextFormField(
-                  decoration: InputDecoration(alignLabelWithHint: true,
-                  labelText: 'Name',
-                  border: OutlineInputBorder(borderRadius: BorderRadius.circular(8))),
-                validator: (enterName){
-                  if (enterName == null) {
-                    return 'Category name cannot be null';
-                  } else if(enterName.length<3){
-                    return 'Category name cannot be less than 3 characters';
-                  }
-                },
-                ),
-              ),
-              key: _myFormKey,
-              
-              ),
-              ButtonBar(
-                mainAxisSize: MainAxisSize.max,
-                children: [
-                  ElevatedButton(onPressed: (){Navigator.pop(context);}, style: ButtonStyle(backgroundColor: MaterialStateProperty.all(Colors.orangeAccent)), child: Text('Cancel', style: TextStyle(color: Colors.white),)),
-                  ElevatedButton(onPressed: (){}, style: ButtonStyle(backgroundColor: MaterialStateProperty.all(Colors.greenAccent)), child: Text('Save', style: TextStyle(color: Colors.white),)),
-                ],
-              ),
-            ],
-            shape: RoundedRectangleBorder(
-    borderRadius: BorderRadius.circular(18),
-  ),
-          );
-        });
-      }, child: Icon(Icons.add_circle), tooltip: 'Add Category', mini: true,),
-      FloatingActionButton(onPressed: (){
-        showDialog(context: context, builder: (context){
-          return SimpleDialog(
-            title: Text('Add Note', style: TextStyle(color: Theme.of(context).primaryColor),),
-            children: [
-              Form(child: TextFormField(
-                decoration: InputDecoration(alignLabelWithHint: true,
-                labelText: 'Title',
-                border: OutlineInputBorder(borderRadius: BorderRadius.circular(8))),
-              ))
-            ],
-          );
-        });
-      }, child: Icon(Icons.add), tooltip: 'Add Note',),
-    ],),
+        addCategoryDialog(context);
+      }, child: Icon(Icons.add_circle), heroTag: 'Add Category', mini: true,),
+      FloatingActionButton(
+          heroTag: "Add Note",
+          onPressed: () {
+            _addNote(context);
+          },
+          child: Icon(Icons.add),
+      ),
+      ],
+      ),
     body: Container(child: Center(child: Text('data'),),),
     );
+  }
+
+  addCategoryDialog(BuildContext context) {
+      GlobalKey<FormState> _myFormKey = GlobalKey<FormState>();
+      String? categoryName;
+
+    return showDialog(
+        context: context,
+        barrierColor: Colors.black.withOpacity(0.4),
+        barrierDismissible: false,
+        builder: (context){
+        return SimpleDialog(
+          title: Text('Add Category', style: TextStyle(color: Theme.of(context).primaryColor)),
+          children: [
+            Form(
+               key: _myFormKey,
+              child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: TextFormField(
+                onSaved: (catName){
+                  categoryName = catName!;
+                },
+                decoration: InputDecoration(alignLabelWithHint: true,
+                labelText: 'Name',
+                border: OutlineInputBorder(borderRadius: BorderRadius.circular(8))),
+              validator: (enterName){
+                if (enterName == null) {
+                  return 'Category name cannot be null';
+                } else if(enterName.length<3){
+                  return 'Category name cannot be less than 3 characters';
+                }
+              },
+              ),
+            ),
+            
+            ),
+            ButtonBar(
+              mainAxisSize: MainAxisSize.max,
+              children: [
+                ElevatedButton(onPressed: (){Navigator.pop(context);}, style: ButtonStyle(backgroundColor: MaterialStateProperty.all(Colors.orangeAccent)), child: Text('Cancel', style: TextStyle(color: Colors.white),)),
+                ElevatedButton(onPressed: (){
+                  if (_myFormKey.currentState!.validate()) {
+                    _myFormKey.currentState!.save();
+                    dbHelper.categoryCreate(Category(name: categoryName)).then((value) {
+                      if (value>0) {
+                      ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text('Category succesfull added'), duration: Duration(seconds: 3),));                     }
+                    });
+                    Navigator.pop(context);
+                  }
+                },
+                 style: ButtonStyle(backgroundColor: MaterialStateProperty.all(Colors.greenAccent)), child: Text('Save', style: TextStyle(color: Colors.white),)),
+              ],
+            ),
+          ],
+          shape: RoundedRectangleBorder(
+  borderRadius: BorderRadius.circular(18),
+),
+        );
+      });
+  }
+
+  _addNote(BuildContext context) {
+    Navigator.push(context, MaterialPageRoute(builder: (conteat) => AddNote()));
   }
 }
